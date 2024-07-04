ea9b509a812d1384d7b48d5ebf79d5ebbfca7204
vukona-mlab <106059406+vukona-mlab@users.noreply.github.com>
2024-07-04 06:13:17 +0200

    Created task-2.js and added task 2.3 array

---
+++ task-2.js
@@ -0,0 +1,132 @@
+const developers = [
+  {
+    name: "Vee",
+    laptops: [
+      "Dell"
+    ],
+    phones: [
+      "Samsung",
+      "Xiaomi"
+    ],
+    computerSetups: [
+      {
+        brand: "Lenovo",
+        monitors: 1,
+        keyboards: 1,
+        mice: 1,
+        speakers: 1
+      }
+    ]
+  },
+  {
+    name: "Katlego",
+    laptops: [
+      "HP",
+      "Samsung"
+    ],
+    phones: [
+      "Apple",
+      "Samsung",
+      "Tecno",
+      "Samsung"
+    ],
+    computerSetups: [
+      {
+        brand: "Lenovo",
+        monitors: 2,
+        keyboards: 1,
+        mice: 1,
+        speakers: 2
+      }, 
+      {
+        brand: "Dell",
+        monitors: 1,
+        keyboards: 1,
+        mice: 1,
+        speakers: 1
+      }
+    ]
+  },
+  {
+    name: "Rethabile",
+    laptops: [
+      "Samsung"
+    ],
+    phones: [
+      "Samsung",
+      "Huawei",
+      "Poco"
+    ],
+    computerSetups: [
+      {
+        brand: "Asus",
+        monitors: 1,
+        keyboards: 1,
+        mice: 1,
+        speakers: 1
+      }, 
+      {
+        brand: "Acer",
+        monitors: 1,
+        keyboards: 1,
+        mice: 1,
+        speakers: 2
+      }
+    ]
+  },
+  {
+    name: "Gift",
+    laptops: [],
+    phones: [
+      "Samsung"
+    ],
+    computerSetups: [
+      {
+        brand: "Acer",
+        monitors: 3,
+        keyboards: 1,
+        mice: 1,
+        speakers: 2
+      }, 
+      {
+        brand: "HP",
+        monitors: 2,
+        keyboards: 1,
+        mice: 1,
+        speakers: 2
+      }
+    ]
+  },
+  {
+    name: "Thokozile",
+    laptops: [
+      "Lenovo"
+    ],
+    phones: [
+      "Apple"
+    ],
+    computerSetups: [
+      {
+        brand: "Dell",
+        monitors: 1,
+        keyboards: 1,
+        mice: 1,
+        speakers: 2
+      }, 
+      {
+        brand: "Asus",
+        monitors: 1,
+        keyboards: 0,
+        mice: 1,
+        speakers: 1
+      }, 
+      {
+        brand: "Dell",
+        monitors: 1,
+        keyboards: 1,
+        mice: 1,
+        speakers: 1
+      }
+    ]
+  }
+]
